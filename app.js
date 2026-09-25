9345a18a7e0de80e4f2f190942a068c550d6aa83
Cacau4524 <gonzagalimac@gmail.com>
2026-09-25 13:51:53 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    alert(`Quantidade: ${quantidade}`)
+    alert(`Do numero: ${de}`)
+    alert(`Ate o numero: ${ate}`)
+
+     let sorteados = [];
+     let numero;
+     
+     for (let i = 0; i < quantidade ; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+     }
+
+  let resultado = document.getElementById('resultado');
+  resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
+ 
+}
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(numero) 
+}
